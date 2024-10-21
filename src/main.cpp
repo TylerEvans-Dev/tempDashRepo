28f25a949c07d48f1d7bd3b634d3a5b3f4b9a68c
Tyler E <evstye@gmail.com>
2024-10-20 23:36:38 -0600

    attempt to work with cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream> 
+#include <wiringPi.h>
 
 int main(){
 
 std::cout << "hello world \n";
+constexpr int pin = 25;
+//if(wiringPiSetup() == -1){
+//	std::cout << "did not setup\n ";
+//}
+
+while(1){
+
+digitalWrite(0, 1);
+delay(500);
+digitalWrite(0, 0);
+std::cout << "is working\n ";
+delay(500);
+}
 
 std::cin.get();
 return 0;
